a896df9102c817a3d457f8554a2eac94da3ea563
gozala <rfobic@gmail.com>
2016-08-19 21:54:37 -0700

    Adapt fork to flow needs.

--- options.js
+++ options.js
@@ -4,13 +4,13 @@ var pkg = require('./package.json')
 
 module.exports = {
   bugs: pkg.bugs.url,
-  cmd: 'standard-react',
+  cmd: 'standard-flow',
   eslint: eslint,
   eslintConfig: {
     configFile: path.join(__dirname, 'eslintrc.json')
   },
-  formatter: '`standard-format` is no longer included with `standard`. Try using --fix instead. Or, install `standard-format` separately: "npm install -g standard-format"',
+  formatter: '`standard-format` is no longer included with `standard`. Try using --fix instead.',
   homepage: pkg.homepage,
-  tagline: 'Use JavaScript Standard Style (React)',
+  tagline: 'Use JavaScript Standard Style (with Flow types)',
   version: pkg.version
 }
